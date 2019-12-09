48a2fcaa3e7be81a23ba80a72824ad72dd3ad854
esuEichi <AloneWare94@gmail.com>
2019-12-09 18:54:41 +0900

    change eval method for competition 2

--- web/app/Http/Controllers/EvaluationController.php
+++ web/app/Http/Controllers/EvaluationController.php
@@ -17,11 +17,13 @@ class EvaluationController extends Controller
 
         $file = $this->getUploadedFileFromRequest($request);
         $this->validFile($file);
-        $score = $this->evaluate($file);
 
-        $result['score'] = $score;
         $result['user_id'] = $request["user_id"];
         $result['competition_id'] = $request["competition_id"];
+
+//        $result = $this->evaluate($result, $file);
+        $result = $this->evaluateCompetition2($result, $file);
+
         $result->save();
 
         $this->redirectToResult($request->competition_id, $result);
@@ -43,23 +45,118 @@ class EvaluationController extends Controller
         return true;
     }
 
-    private function evaluate($file){
+    private function evaluate($result, $file){
         $answer = new \SplFileObject(database_path('answer.csv'));
         $answer = $this->file2array($answer);
-        $result = $this->file2array($file);
+        $result_array = $this->file2array($file);
 
+        $result['score'] = $this->calcAccuracy($answer, $result_array);
+
+        return $result; 
+        //return ($score/$i) * 100;
+    }
+
+    private function evaluateCompetition2($result, $file){
+        $answer = new \SplFileObject(database_path('answer.csv'));
+        $answer = $this->csv2array($answer);
+        $answer_index_0 = $this->getDataByCsvWithIndex($answer,0);
+        $answer_index_1 = $this->getDataByCsvWithIndex($answer,1);
+//        $answer_index_2 = $this->getDataByCsvWithIndex($answer,2);
+//        $answer_index_3 = $this->getDataByCsvWithIndex($answer,3);
+//        $answer_index_4 = $this->getDataByCsvWithIndex($answer,4);
+
+        $result_array = $this->file2array($file);
+        $result_index_0 = $this->getDataByCsvWithIndex($result_array, 0);    
+        $result_index_1 = $this->getDataByCsvWithIndex($result_array, 1);    
+//        $result_index_2 = $this->getDataByCsvWithIndex($result_array, 2);    
+//        $result_index_3 = $this->getDataByCsvWithIndex($result_array, 3);    
+//        $result_index_4 = $this->getDataByCsvWithIndex($result_array, 4);    
+
+//       echo "<pre>";
+//       var_dump($answer_index_0[0]);
+//       var_dump($answer_index_1[0][0]);
+//       var_dump($result_index_0[0]);
+//       var_dump($result_index_1[0][0]);
+
+//       echo "</pre>";
+//       exit();
+
+
+        $result['opt_score1'] = $this->calcFMeasureWithArrays($answer_index_0, $result_index_0);
+        $result['opt_score2'] = $this->calcFMeasureWithArrays($answer_index_1, $result_index_1);
+//        $result['opt_score3'] = $this->calcFMeasureWithArrays($answer_index_2, $result_index_2);
+//        $result['opt_score4'] = $this->calcFMeasureWithArrays($answer_index_3, $result_index_3);
+//        $result['opt_score5'] = $this->calcFMeasureWithArrays($answer_index_4, $result_index_4);
+
+        $result['score'] = ($result['opt_score1'] + $result['opt_score2'] ) / 2;
+
+        return $result; 
+        //return ($score/$i) * 100;
+    }
+
+
+    private function calcAccuracy($answer, $result_array){
         $i = 0;
         $score = 0;
         while($i < count($answer)){
-            if($i < count($result) ){
-                if((int)$answer[$i] == (int)$result[$i][0]){
+            if($i < count($result_array) ){
+                if((int)$answer[$i] == (int)$result_array[$i][0]){
                     $score++;
                 }
             }
             $i++;
         }
-
         return ($score/$i) * 100;
+    }
+
+    private function calcFMeasureWithPrecisionAndRecall($precision, $recall){
+
+        return (2 * $recall * $precision) / ($recall + $precision);
+    }
+
+    private function calcFMeasureWithArrays($answerArr, $resultArr){
+        $recall = $this->calcRecall($answerArr, $resultArr);
+        $precision = $this->calcPrecision($answerArr, $resultArr);
+
+        return $this->calcFMeasureWithPrecisionAndRecall($precision, $recall);
+    }
+
+    private function calcPrecision($answer, $result_array){
+        $i = 0;
+        $score = 0;
+        $results = 0;
+        while($i < count($answer)){
+            if($i < count($result_array) ){
+                if((int)$result_array[$i] == 1){
+                    $results++;
+                    if((int)$answer[$i] == (int)$result_array[$i]){
+                        $score++;
+                    }
+                }
+            }
+            $i++;
+        }
+        return ($score/$results);
+
+    }
+
+    private function calcRecall($answer, $result_array){
+        $i = 0;
+        $score = 0;
+        $results = 0;
+        while($i < count($answer)){
+            if($i < count($result_array)) {
+                if((int)$answer[$i] == 1){
+                    $results++;
+
+                    if((int)$answer[$i] == (int)$result_array[$i]){
+                        $score++;
+                    }    
+                }
+            }
+            $i++;
+        }
+        return ($score/$results);
     }
 
     private function file2array($file){
@@ -68,6 +165,29 @@ class EvaluationController extends Controller
             array_push($arr, $line);
         }
         return $arr;
+
+    }
+
+    private function csv2array($file){
+        $temp = $this->file2array($file);
+        $arr = Array();
+
+        foreach($temp as $line){
+            array_push($arr, explode(',', $line));
+        }
+
+        return $arr;
+
+    }
+
+    private function getDataByCsvWithIndex($csv_array, $index){
+        $arr = Array();
+        foreach($csv_array as $data){
+            array_push($arr, $data[$index]);
+        }
+        
+        return $arr;
+
     }
 
     private function redirectToResult($competition_id, $result){
